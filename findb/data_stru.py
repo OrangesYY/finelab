d24ec86bc7168198ed867fbd8d2ba9a51a3c3515
Sean Luo <luoyusang2007@hotmail.com>
2018-11-11 15:23:48 +0800

    add code for path testing

--- findb/data_stru.py
+++ findb/data_stru.py
@@ -1,9 +1,19 @@
 import sys, os, json, copy
 
+
 class Business:
     raw_dict = {}
     formatted_dict = {}
     def __init__(self,arg = None):
+        # Code for testing paths
+        # print("__file__=%s" % __file__)
+        # print("os.path.realpath(__file__)=%s" % os.path.realpath(__file__))
+        # print("os.path.dirname(os.path.realpath(__file__))=%s" % os.path.dirname(os.path.realpath(__file__)))
+        # print("os.path.split(os.path.realpath(__file__))=%s" % os.path.split(os.path.realpath(__file__))[0])
+        # print("os.path.abspath(__file__)=%s" % os.path.abspath(__file__))
+        # print("os.getcwd()=%s" % os.getcwd())
+        # print("sys.path[0]=%s" % sys.path[0])
+        # print("sys.argv[0]=%s" % sys.argv[0])
         if type(arg) == dict:
             self.raw_dict = arg
             self.format_bussiness()
